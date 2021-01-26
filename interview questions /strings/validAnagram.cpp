aaee774cfc072a1743253760232cfd58e7e27d39
Julia <iulia.golicova@gmail.com>
2021-01-25 19:05:07 -0500

    valid anagram done

---
+++ interview questions /strings/validAnagram.cpp
@@ -0,0 +1,32 @@
+//c++ -std=c++11 validAnagram.cpp 
+
+#include <iostream>
+#include <stack> 
+#include "unordered_map"
+
+using namespace std; 
+
+bool isAnagram(string s, string t) {
+    if(s.length() != t.length()) return false; 
+
+    int n = s.length(); 
+    unordered_map<char, int> counts; 
+    for(int i = 0; i < n; i++) {
+        counts[s[i]]++;
+        counts[t[i]]--;
+    }
+
+    for(auto i : counts) {
+        if(i.second == 1) return false; 
+    }
+    return true;
+}
+
+int main() {
+    // 1
+    cout << isAnagram("anagram", "nagaram") << endl;
+    // 0 
+    cout << isAnagram("rat", "car") << endl;
+    // 1
+    cout << isAnagram("a", "a") << endl;
+}
